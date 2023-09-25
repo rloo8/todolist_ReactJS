27d146cbd30816cc2426c4ad60626c449d69c016
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-09-26 01:22:29 +0900

    #1 todolist

---
+++ ToDoList.tsx
@@ -0,0 +1,28 @@
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  toDo: string;
+}
+
+function ToDoList() {
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+
+  const handleValid = (data: IForm) => {
+    console.log("add to do", data.toDo);
+    setValue("toDo", "");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register("toDo", { required: "please write a to do" })}
+          placeholder="Write a to do"
+        />
+        <button>ADD</button>
+      </form>
+    </div>
+  );
+}
+
+export default ToDoList;
